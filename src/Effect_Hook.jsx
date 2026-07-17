4b0df6338f1be5e221be894e4a0cac79e18cd3e1
DEVJI708 <aniyaliyadevji1@gmail.com>
2026-07-17 14:34:05 +0530

    17/7 useEffect changes

--- src/Effect_Hook.jsx
+++ src/Effect_Hook.jsx
@@ -1,23 +1,35 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 export default function Effect_Hook() {
   const [count, setCount] = useState(0);
+  const [quantity, setQuantity] = useState(10);
+
+  const prevCount = useRef(count);
 
   useEffect(() => {
-    console.log("Count Changed:", count);
+    if (count > prevCount.current) {
+      setQuantity((prev) => Math.max(prev - 1, 0));
+    }
+
+    prevCount.current = count;
   }, [count]);
 
   return (
     <div>
-      <h1>useEffect Hook Example</h1>
-
       <h2>Count: {count}</h2>
+      <h2>Quantity: {quantity}</h2>
 
       <button onClick={() => setCount(count + 1)}>
         Increment
       </button>
 
-      <button onClick={() => setCount(count - 1)}>
+      <button
+        onClick={() => {
+          if (count > 0) {
+            setCount(count - 1);
+          }
+        }}
+      >
         Decrement
       </button>
     </div>
