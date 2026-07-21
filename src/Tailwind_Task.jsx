5feb1cf29c98c49893fa0b6c2f3f25762fe8edc6
DEVJI708 <aniyaliyadevji1@gmail.com>
2026-07-21 15:05:28 +0530

    21/7 state lifting up

--- src/Tailwind_Task.jsx
+++ src/Tailwind_Task.jsx
@@ -1,29 +1,25 @@
-import Picture from './assets/birds.jpg'
-export default function Tailwind_Task(){
-    return (
-       <header class="w-full bg-sky-200">
-        <div class="max-w-auto px-10 py-5 flex items-center justify-between">
-            <div class="">
-                <img src={Picture} height="100" width="100" alt="birds image"></img>
-            </div>
-                <nav>
-                <ul class="flex items-center space-x-8 text-gray-700 font-medium">
-                    <li><a href="#" class="hover:text-blue-600 transition">Home</a></li>
-                    <li><a href="#" class="hover:text-blue-600 transition">About</a></li>
-                    <li><a href="#" class="hover:text-blue-600 transition">Services</a></li>
-                    <li><a href="#" class="hover:text-blue-600 transition">Contact</a></li>
-                </ul>
-                </nav>
+import Picture from "./assets/birds.jpg";
+export default function Tailwind_Task() {
+  return (
+    <header class="w-full bg-sky-200">
+      <div class=" px-10 py-3 flex items-center justify-between">
+        <div class="">
+          <img src={Picture} height="100" width="100" alt="birds image"></img>
+        </div>
+        <nav class="items-center mt-8">
+          <ul class="flex items-center space-x-20  text-blue-800 font-medium">
+            <li><p class="hover:bg-amber-300 no-underline">Home</p></li>
+            <li><p href="#" class="hover:bg-amber-300 text-blue-800">About</p></li>
+            <li><p href="#" class="hover:bg-amber-300 text-blue-800">Services</p></li>
+            <li><p href="#" class="hover:bg-amber-300  text-blue-800">Contact</p></li>
+          </ul>
+        </nav>
+        <div>
+          <button class="bg-blue-600 text-white px-5 py-2 rounded  hover:scale-140">Get Started</button>
+        </div>
+      </div>
+    </header>
 
-   
-    <div>
-      <button class="bg-blue-600 text-white px-5 py-2 rounded-lg hover:bg-blue-700 transition">
-        Get Started
-      </button>
-    </div>
-
-  </div>
-</header>
-)
+    
+  );
 }
-
